3993ae36d83a4acde405a791e9cbe80671a36bcc
Pierre Hedkvist <pierre.hedkvist@gmail.com>
2023-03-16 22:13:26 +0100

    Add tests for dragging elements

--- src/context/MouseEvents.test.tsx
+++ src/context/MouseEvents.test.tsx
@@ -1,4 +1,4 @@
-import { fireEvent, render } from "@testing-library/react";
+import { fireEvent, render, screen } from "@testing-library/react";
 import Canvas from "../components/Canvas/Canvas";
 import Toolbar from "../components/Toolbar/Toolbar";
 import { AppStateProvider } from "./AppState";
@@ -15,26 +15,85 @@ const renderWrapper = () => {
   );
 };
 
+const mouseDragEvent = (
+  element: HTMLElement,
+  from: {
+    x: number;
+    y: number;
+  },
+  to: {
+    x: number;
+    y: number;
+  }
+) => {
+  fireEvent.mouseDown(element, {
+    clientX: from.x,
+    clientY: from.y,
+  });
+  fireEvent.mouseMove(element, {
+    clientX: to.x,
+    clientY: to.y,
+  });
+  fireEvent.mouseUp(element, {
+    clientX: to.x,
+    clientY: to.y,
+  });
+};
+
 describe("MouseEvents", () => {
   it("Should create a rect", () => {
-    // TODO: Create a rect by firing a bunch of mouse events :)
-    const screen = renderWrapper();
+    renderWrapper();
 
     fireEvent.click(screen.getByText("Rect"));
 
-    fireEvent.mouseDown(screen.getByTestId("canvas"), {
-      clientX: 100,
-      clientY: 100,
-    });
-    fireEvent.mouseMove(screen.getByTestId("canvas"), {
-      clientX: 200,
-      clientY: 200,
-    });
-    fireEvent.mouseUp(screen.getByTestId("canvas"), {
-      clientX: 200,
-      clientY: 200,
-    });
+    const canvas = screen.getByTestId("canvas");
+    mouseDragEvent(canvas, { x: 100, y: 100 }, { x: 200, y: 200 });
 
     screen.getByTestId("rect-svg");
+  });
+
+  it("Should move an existing rect", () => {
+    renderWrapper();
+
+    fireEvent.click(screen.getByText("Rect"));
+
+    const canvas = screen.getByTestId("canvas");
+    mouseDragEvent(canvas, { x: 100, y: 100 }, { x: 200, y: 200 });
+
+    const rect = screen.getByTestId("rect-svg");
+
+    mouseDragEvent(rect, { x: 200, y: 200 }, { x: 300, y: 300 });
+
+    expect(Number(rect.getAttribute("x"))).toEqual(200);
+    expect(Number(rect.getAttribute("y"))).toEqual(200);
+  });
+
+  it("Should create a circle", () => {
+    // TODO: Create a rect by firing a bunch of mouse events :)
+    const screen = renderWrapper();
+
+    fireEvent.click(screen.getByText("Circle"));
+
+    const canvas = screen.getByTestId("canvas");
+    mouseDragEvent(canvas, { x: 100, y: 100 }, { x: 200, y: 200 });
+
+    expect(screen.getByTestId("circle-svg")).toBeDefined();
+  });
+
+  it("Should move a circle", () => {
+    // TODO: Create a rect by firing a bunch of mouse events :)
+    const screen = renderWrapper();
+
+    fireEvent.click(screen.getByText("Circle"));
+
+    const canvas = screen.getByTestId("canvas");
+    mouseDragEvent(canvas, { x: 100, y: 100 }, { x: 200, y: 200 });
+
+    const circle = screen.getByTestId("circle-svg");
+
+    mouseDragEvent(circle, { x: 200, y: 200 }, { x: 300, y: 300 });
+
+    expect(Number(circle.getAttribute("cx"))).toEqual(250);
+    expect(Number(circle.getAttribute("cy"))).toEqual(250);
   });
 });
