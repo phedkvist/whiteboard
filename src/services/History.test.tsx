8d09fa627afb2cacbec7bc5bb9d6cb15263a5d6a
Pierre Hedkvist <pierre.hedkvist@gmail.com>
2023-03-18 21:42:00 +0100

    Add tests for concurrent updates to history class

--- src/services/History.test.tsx
+++ src/services/History.test.tsx
@@ -3,9 +3,9 @@ import { It, mock, when } from "strong-mock";
 import { AppState } from "../types";
 import { createRectAction, updateRectAction } from "./Actions/Rect";
 import { act, renderHook, waitFor } from "@testing-library/react";
-import { useState } from "react";
+import { useMemo, useState } from "react";
 
-const createRect = createRectAction(0, 0, 1, "unique-id", "user-id");
+const createRect = createRectAction(0, 0, 1, "unique-id", "b");
 const updatedRect = updateRectAction(
   createRect.object,
   false,
@@ -25,7 +25,7 @@ const useHistoryRender = (
   }
 ): [History, AppState] => {
   const [appState, setAppState] = useState<AppState>(initialState);
-  const [history] = useState(new History(appState, setAppState, ws));
+  const history = useMemo(() => new History(appState, setAppState, ws), []);
 
   return [history, appState];
 };
@@ -75,6 +75,86 @@ describe("History", () => {
     waitFor(() => {
       expect(state).toEqual({
         elements: { ["unique-id"]: updatedRect.object },
+        cursors: {},
+      });
+    });
+  });
+
+  it("Should reject updating existing changes if version is behind", async () => {
+    const mockWs = mock<WebSocket>();
+    when(() => mockWs.addEventListener("message", It.isAny(), false))
+      .thenReturn()
+      .anyTimes();
+    when(() => mockWs.addEventListener("open", It.isAny(), false))
+      .thenReturn()
+      .anyTimes();
+    when(() => mockWs.addEventListener("close", It.isAny(), false))
+      .thenReturn()
+      .anyTimes();
+
+    const { result } = renderHook(() => useHistoryRender(mockWs, appStateStub));
+    const [history, state] = result.current;
+
+    act(() => {
+      history.addLocalChange(
+        {
+          ...updatedRect,
+          object: {
+            ...updatedRect.object,
+            userVersion: {
+              version: 0,
+              userId: updatedRect.object.userVersion.userId,
+            },
+          },
+        },
+        true
+      );
+    });
+    waitFor(() => {
+      expect(state).toEqual({
+        elements: { ["unique-id"]: createRect.object },
+        cursors: {},
+      });
+    });
+  });
+
+  it("Should updating concurrent change if user id is lexicographically higher", async () => {
+    const mockWs = mock<WebSocket>();
+    when(() => mockWs.addEventListener("message", It.isAny(), false))
+      .thenReturn()
+      .anyTimes();
+    when(() => mockWs.addEventListener("open", It.isAny(), false))
+      .thenReturn()
+      .anyTimes();
+    when(() => mockWs.addEventListener("close", It.isAny(), false))
+      .thenReturn()
+      .anyTimes();
+
+    const { result } = renderHook(() => useHistoryRender(mockWs, appStateStub));
+    const [history, state] = result.current;
+
+    const concurrentObject = {
+      ...updatedRect.object,
+      userVersion: {
+        version: 1,
+        userId: "a",
+      },
+    };
+
+    act(() => {
+      history.addLocalChange(
+        {
+          ...updatedRect,
+          object: concurrentObject,
+        },
+        true
+      );
+    });
+    waitFor(() => {
+      expect(state).toEqual({
+        elements: {
+          ["unique-id"]: concurrentObject,
+        },
         cursors: {},
       });
     });
