782fd50ff13cbd68dcd229a66093d2ceaa68f08f
Pierre Hedkvist <pierre.hedkvist@gmail.com>
2023-03-19 19:11:58 +0100

    Add tests for receiving cursor updates

--- src/services/History.test.tsx
+++ src/services/History.test.tsx
@@ -1,9 +1,10 @@
-import History from "./History";
+import History, { CursorEvent } from "./History";
 import { It, mock, when } from "strong-mock";
-import { AppState } from "../types";
+import { AppState, Cursor } from "../types";
 import { createRectAction, updateRectAction } from "./Actions/Rect";
 import { act, renderHook, waitFor } from "@testing-library/react";
 import { useMemo, useState } from "react";
+import { cursorStub, eventStub } from "../stubs";
 
 const createRect = createRectAction(0, 0, 1, "unique-id", "b");
 const updatedRect = updateRectAction(
@@ -191,5 +192,34 @@ describe("History", () => {
     );
     const [history] = result.current;
     history.sendCursor(100, 100, "2023-03-19T09:31:49.722Z");
+  });
+
+  it("Should receive cursor updates and updates internal state", async () => {
+    const mockWs = mock<WebSocket>();
+    when(() => mockWs.addEventListener("message", It.isAny(), false))
+      .thenReturn()
+      .anyTimes();
+    when(() => mockWs.addEventListener("open", It.isAny(), false))
+      .thenReturn()
+      .anyTimes();
+    when(() => mockWs.addEventListener("close", It.isAny(), false))
+      .thenReturn()
+      .anyTimes();
+
+    const { result } = renderHook(() =>
+      useHistoryRender(mockWs, appStateStub, "unique-user-id")
+    );
+    const [history, state] = result.current;
+
+    await history.onMessage(eventStub);
+
+    waitFor(() => {
+      expect(state).toEqual({
+        elements: {},
+        cursors: {
+          [cursorStub.id]: cursorStub,
+        },
+      });
+    });
   });
 });
---
+++ src/stubs.ts
@@ -0,0 +1,74 @@
+import { CursorEvent } from "./services/History";
+import { Cursor } from "./types";
+
+export const cursorStub: Cursor = {
+  id: "unique-id",
+  position: {
+    x: 0,
+    y: 0,
+  },
+  lastUpdated: "2023-03-19T09:31:49.722Z",
+  color: "red",
+};
+export const cursorEventStub: CursorEvent = {
+  type: "cursor",
+  data: [cursorStub],
+};
+
+export const eventStub: MessageEvent = {
+  data: {
+    text: () => JSON.stringify(cursorEventStub),
+  },
+  lastEventId: "",
+  origin: "",
+  ports: [],
+  source: null,
+  initMessageEvent: function (
+    type: string,
+    bubbles?: boolean | undefined,
+    cancelable?: boolean | undefined,
+    data?: any,
+    origin?: string | undefined,
+    lastEventId?: string | undefined,
+    source?: MessageEventSource | null | undefined,
+    ports?: MessagePort[] | undefined
+  ): void {
+    throw new Error("Function not implemented.");
+  },
+  bubbles: false,
+  cancelBubble: false,
+  cancelable: false,
+  composed: false,
+  currentTarget: null,
+  defaultPrevented: false,
+  eventPhase: 0,
+  isTrusted: false,
+  returnValue: false,
+  srcElement: null,
+  target: null,
+  timeStamp: 0,
+  type: "",
+  composedPath: function (): EventTarget[] {
+    throw new Error("Function not implemented.");
+  },
+  initEvent: function (
+    type: string,
+    bubbles?: boolean | undefined,
+    cancelable?: boolean | undefined
+  ): void {
+    throw new Error("Function not implemented.");
+  },
+  preventDefault: function (): void {
+    throw new Error("Function not implemented.");
+  },
+  stopImmediatePropagation: function (): void {
+    throw new Error("Function not implemented.");
+  },
+  stopPropagation: function (): void {
+    throw new Error("Function not implemented.");
+  },
+  AT_TARGET: 0,
+  BUBBLING_PHASE: 0,
+  CAPTURING_PHASE: 0,
+  NONE: 0,
+};
