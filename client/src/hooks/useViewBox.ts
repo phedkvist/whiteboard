c760eaadd70941c10ee890fef7394cc1a2fbc3ea
Pierre Hedkvist <pierre.hedkvist@gmail.com>
2023-08-05 15:46:51 +0200

    Fix issue with viewbox not adjusting properly when resizing the window

--- client/src/hooks/useViewBox.ts
+++ client/src/hooks/useViewBox.ts
@@ -1,21 +1,39 @@
-import { useState } from "react";
+import { useLayoutEffect, useState } from "react";
 import { ViewBox, initialViewBox } from "../types";
+import useWindowDimensions from "./useWindowDimensions";
+
+const isWindowDimensionsUpdated = (
+  w1: ReturnType<typeof useWindowDimensions>,
+  w2: ReturnType<typeof useWindowDimensions>
+) => {
+  return w1.height !== w2.height || w1.width !== w2.width;
+};
 
 export const useViewBox = (): [
   ViewBox,
   React.Dispatch<React.SetStateAction<ViewBox>>
 ] => {
   const [viewBox, setViewBox] = useState<ViewBox>(initialViewBox(window));
+  const currentWindowDimension = useWindowDimensions();
 
-  // TODO: Make sure this is not interering with zooming in/out
-  // const { height, width } = useWindowDimensions();
-  // const h = viewBox.scale * height;
-  // const w = viewBox.scale * width;
-  // useEffect(() => {
-  //   if (viewBox.h !== h || viewBox.w !== w) {
-  //     setViewBox({ ...viewBox, h, w });
-  //   }
-  // }, [h, w, viewBox, setViewBox]);
+  const [windowDimension, setWindowDimensions] = useState(
+    currentWindowDimension
+  );
+
+  useLayoutEffect(() => {
+    // This will adjust the viewBox when the window size changes
+    if (isWindowDimensionsUpdated(windowDimension, currentWindowDimension)) {
+      const diffH = windowDimension.height - currentWindowDimension.height;
+      const diffW = windowDimension.width - currentWindowDimension.width;
+
+      setViewBox({
+        ...viewBox,
+        h: viewBox.h - diffH * viewBox.scale,
+        w: viewBox.w - diffW * viewBox.scale,
+      });
+      setWindowDimensions(currentWindowDimension);
+    }
+  }, [viewBox, setViewBox, currentWindowDimension]);
 
   return [viewBox, setViewBox];
 };
